7023fd1970575fc46e2b3073887d110b3cce300d
C. Liam Brown <liam@cliambrown.com>
2024-04-03 17:37:23 -0400

    Add delete external item

--- src/views/ExternalItemPageView.vue
+++ src/views/ExternalItemPageView.vue
@@ -43,8 +43,13 @@ function toggleArchived() {
   handleUpdate();
 }
 
-function deleteItem() {
-  
+async function deleteItem() {
+  if (!item.value || !item.value.id) return false;
+  const confirmed = await confirm('Are you sure you want to delete this item?', { title: 'Playa - Delete Item', type: 'warning' });
+  if (!confirmed) return false;
+  await item.value.delete();
+  store.loading_msg = 'Item deleted';
+  store.router.push({ name: 'home' });
 }
 
 async function handleUpdate() {
@@ -71,7 +76,6 @@ watch(
     if (itemID && !store.external_item_ids.includes(itemID)) {
       store.external_items[itemID] = item;
       store.external_item_ids.push(itemID);
-      store.sortExternalItems();
     }
   }
 )
@@ -208,7 +212,7 @@ onBeforeUnmount(() => {
         </span>
       </Button>
       
-      <Button variant="delete" :disabled="store.loading">
+      <Button variant="delete" :disabled="store.loading || !item.id" @click="deleteItem">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
           <path fill-rule="evenodd" d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z" clip-rule="evenodd" />
         </svg>
