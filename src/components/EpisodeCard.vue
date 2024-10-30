de4a5c4c3ed931331bfb14b7aa6151f6c63d9bd1
C. Liam Brown <liam@cliambrown.com>
2024-10-30 09:24:18 -0400

    Fix filenames not showing in episode cards due to text-ellipsis

--- src/components/EpisodeCard.vue
+++ src/components/EpisodeCard.vue
@@ -135,7 +135,7 @@ watch(
           </Button>
         </div>
         
-        <div class="mt-1 overflow-hidden whitespace-nowrap text-ellipsis">
+        <div class="mt-1 overflow-hidden whitespace-nowrap">
           <span class="inline-flex items-center gap-2 mr-4 text-gray-600">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 text-gray-400">
               <path fill-rule="evenodd" d="M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8Zm7.75-4.25a.75.75 0 0 0-1.5 0V8c0 .414.336.75.75.75h3.25a.75.75 0 0 0 0-1.5h-2.5v-3.5Z" clip-rule="evenodd" />
